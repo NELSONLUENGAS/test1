4acb0e24172b79cd47e3b98924446b4d27345d3e
NELSON ANDRES LUENGAS SILVA <90053867+NELSONLUENGAS@users.noreply.github.com>
2024-10-04 15:59:55 -0500

    Update students controller

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -3,27 +3,77 @@ const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updat
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
     //write your code
+    try {
 
+
+        const { name, section, class: className, roll } = req.query;
+
+        const students = await getAllStudents({ name, section, className, roll });
+
+        res.json({ students });
+    } catch (error) {
+        throw error
+    }
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
     //write your code
 
+    try {
+
+
+        const newStudent = await addNewStudent({ ...req.body })
+
+        res.send(newStudent)
+    } catch (error) {
+        throw error
+    }
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
     //write your code
+    try {
 
+
+        const id = req.params
+        const fieldsToUpdate = req.body
+
+        const response = await updateStudent({ id, ...fieldsToUpdate })
+
+        res.send(response)
+    } catch (error) {
+        throw error
+    }
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
     //write your code
+    try {
 
+
+        const id = req.params
+
+        const studentDetail = await getStudentDetail(id)
+
+        res.json(studentDetail)
+    } catch (error) {
+        throw error
+    }
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
     //write your code
+    try {
 
+
+        const userId = req.params
+        const { reviewerId, status } = req.body
+
+        const response = await setStudentStatus({ userId, reviewerId, status })
+        res.send(response)
+    } catch (error) {
+        throw error
+    }
 });
 
 module.exports = {
